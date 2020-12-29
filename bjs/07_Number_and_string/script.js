932a4ce18858f903eadcdf930bb4b82db7677f15
cit-rus22 <biolan93@gmail.com>
2020-12-29 22:21:22 +0300

    if else

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -3,104 +3,180 @@ let operation = null;
 
 const inputWindow = document.getElementById('inputWindow');
 
+inputWindow.value = '0';
+
 
 document.getElementById('btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
-    inputWindow.value = '';
+    inputWindow.value = '0';
 })
 
+// Цифры
+
 document.getElementById('btn_1').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '1';
 })
 
 document.getElementById('btn_2').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '2';
 })
 
 document.getElementById('btn_3').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '3';
 })
 
 document.getElementById('btn_4').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '4';
 })
 
 document.getElementById('btn_5').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '5';
 })
 
 document.getElementById('btn_6').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '6';
 })
 
 document.getElementById('btn_7').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '7';
 })
 
 document.getElementById('btn_8').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '8';
 })
 
 document.getElementById('btn_9').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '9';
 })
 
 document.getElementById('btn_0').addEventListener('click', function () {
+    if (inputWindow.value === '0') {
+        inputWindow.value = '';
+    }
     inputWindow.value += '0';
 })
 
+// Операции
+
 document.getElementById('btn_sum').addEventListener('click', function () {
-    lastOperand = parseInt(inputWindow.value);
+    lastOperand = parseFloat(inputWindow.value);
     operation = 'sum';
+    console.log(lastOperand);
     inputWindow.value = '';
 })
 
 document.getElementById('btn_def').addEventListener('click', function () {
-    lastOperand = parseInt(inputWindow.value);
+    lastOperand = parseFloat(inputWindow.value);
     operation = 'def';
     inputWindow.value = '';
 })
 
 document.getElementById('btn_multi').addEventListener('click', function () {
-    lastOperand = parseInt(inputWindow.value);
+    lastOperand = parseFloat(inputWindow.value);
     operation = 'multi';
     inputWindow.value = '';
 })
 
 document.getElementById('btn_div').addEventListener('click', function () {
-    lastOperand = parseInt(inputWindow.value);
+    lastOperand = parseFloat(inputWindow.value);
     operation = 'div';
     inputWindow.value = '';
 })
 
+document.getElementById('btn_dot').addEventListener('click', function () {
+    lastOperand = parseFloat(inputWindow.value);
+    if (inputWindow.value.includes('.') == false) {
+        inputWindow.value += ".";}
+})
+
+document.getElementById('btn_minus').addEventListener('click', function () {
+    lastOperand = parseFloat(inputWindow.value);
+    operation = 'minus';
+    inputWindow.value = '';
+    if (operation === 'minus') {
+        const result = -lastOperand;
+        operation = null;
+        lastOperand = 0;
+        inputWindow.value = result;
+    }
+})
+
+document.getElementById('btn_sqrt').addEventListener('click', function () {
+    lastOperand = parseFloat(inputWindow.value);
+    operation = 'sqrt';
+    const resultSqrt = Math.sqrt(lastOperand);
+    let tryOutput1 = (`Корень из ${lastOperand} равен ${resultSqrt}`);
+    document.querySelector('#duplicateField').innerHTML = ` ${tryOutput1}`;
+    operation = null;
+    lastOperand = 0;
+    inputWindow.value = resultSqrt;
+})
+
+// Равно
+
 document.getElementById('btn_eq').addEventListener('click', function () {
     switch (operation) {
         case 'sum':
-            const resultSum = lastOperand + parseInt(inputWindow.value);
+            const resultSum = lastOperand + parseFloat(inputWindow.value);
+            let tryOutput2 = (`${lastOperand} + ${inputWindow.value} = ${resultSum}`);
+            document.querySelector('#duplicateField').innerHTML = ` ${tryOutput2}`;
             operation = null;
-            lastOPerand = 0;
+            console.log(lastOperand);
+            lastOperand = 0;
             inputWindow.value = resultSum;
             break;
         case 'def':
-            const resultDef = lastOperand - parseInt(inputWindow.value);
+            const resultDef = lastOperand - parseFloat(inputWindow.value);
+            let tryOutput3 = (`${lastOperand} - ${inputWindow.value} = ${resultDef}`);
+            document.querySelector('#duplicateField').innerHTML = ` ${tryOutput3}`;
             operation = null;
-            lastOPerand = 0;
+            lastOperand = 0;
             inputWindow.value = resultDef;
             break;
         case 'multi':
-            const resultMulti = lastOperand * parseInt(inputWindow.value);
+            const resultMulti = lastOperand * parseFloat(inputWindow.value);
+            let tryOutput4 = (`${lastOperand} * ${inputWindow.value} = ${resultMulti}`);
+            document.querySelector('#duplicateField').innerHTML = ` ${tryOutput4}`;
             operation = null;
-            lastOPerand = 0;
+            lastOperand = 0;
             inputWindow.value = resultMulti;
             break;
-            //
         case 'div':
-            const resultDiv = lastOperand / parseInt(inputWindow.value);
+            const resultDiv = lastOperand / parseFloat(inputWindow.value);
+            let tryOutput5 = (`${lastOperand} / ${inputWindow.value} = ${resultDiv}`);
+            document.querySelector('#duplicateField').innerHTML = ` ${tryOutput5}`;
             operation = null;
-            lastOPerand = 0;
+            lastOperand = 0;
             inputWindow.value = resultDiv;
-            break;
-    
+            break;    
     }
 })
+
--- bjs/08_if_else/script.js
+++ bjs/08_if_else/script.js
@@ -1,7 +1,16 @@
-let minValue = parseInt(prompt('Минимальное знание числа для игры','0'));
-let maxValue = parseInt(prompt('Максимальное знание числа для игры','100'));
+let inputMin = parseInt(prompt('Минимальное знание числа для игры', '0'));
+let inputMax = parseInt(prompt('Максимальное знание числа для игры', '100'));
+
+// Проверка на мин/макс и NaN
+minValue = parseInt(inputMin);
+maxValue = parseInt(inputMax);
+minValue = minValue || 0;
+minValue = (minValue < -999) ? -999 : minValue;
+maxValue = maxValue || 100;
+maxValue = (maxValue > 999) ? 999 : maxValue;
+
 alert(`Загадайте любое целое число от ${minValue} до ${maxValue}, а я его угадаю`);
-let answerNumber  = Math.floor((minValue + maxValue) / 2);
+let answerNumber = Math.floor((minValue + maxValue) / 2);
 let orderNumber = 1;
 let gameRun = true;
 
@@ -9,38 +18,124 @@ const orderNumberField = document.getElementById('orderNumberField');
 const answerField = document.getElementById('answerField');
 
 orderNumberField.innerText = orderNumber;
-answerField.innerText = `Вы загадали число ${answerNumber }?`;
+const phraseRandom = Math.round(Math.random() * 3);
+if (phraseRandom == 1) {
+    answerField.innerText = `Вы загадали число ${answerNumber }`
+} else if (phraseRandom == 2) {
+    answerField.innerText = `Наверное это ${answerNumber }`
+} else {
+    answerField.innerText = `Думаю это число ${answerNumber }`
+};
+
+// Заново
 
 document.getElementById('btnRetry').addEventListener('click', function () {
-    minValue = 0;
-    maxValue = 100;
+    inputMin = parseInt(prompt('Минимальное знание числа для игры', '0'));
+    inputMax = parseInt(prompt('Максимальное знание числа для игры', '100'));
+    minValue = parseInt(inputMin);
+    maxValue = parseInt(inputMax);
+    minValue = minValue || 0;
+    minValue = (minValue < -999) ? -999 : minValue;
+    maxValue = maxValue || 100;
+    maxValue = (maxValue > 999) ? 999 : maxValue;
     orderNumber = 0;
+    gameRun = true;
+    let answerNumber = Math.floor((minValue + maxValue) / 2);
+    const phraseRandom = Math.round(Math.random() * 3);
+    if (phraseRandom == 1) {
+        answerField.innerText = `Вы загадали число ${answerNumber }`
+    } else if (phraseRandom == 2) {
+        answerField.innerText = `Наверное это ${answerNumber }`
+    } else {
+        answerField.innerText = `Думаю это число ${answerNumber }`
+    };
+
 })
 
-document.getElementById('btnOver').addEventListener('click', function () {
-    if (gameRun){
-        if (minValue === maxValue){
-            const phraseRandom = Math.round( Math.random());
-            const answerPhrase = (phraseRandom === 1) ?
-                `Вы загадали неправильное число!\n\u{1F914}` :
-                `Я сдаюсь..\n\u{1F92F}`;
 
+// Больше 
+document.getElementById('btnOver').addEventListener('click', function () {
+    if (gameRun) {
+        if (minValue === maxValue) {
+            const phraseRandom = Math.round(Math.random() * 3);
+            let answerText;
+            if (phraseRandom == 1) {
+                answerText = `Что-то не то\n\u{1F60E}`
+            } else if (phraseRandom == 2) {
+                answerText = `Что-то здесь не так \n\u{1F60E}`
+            } else {
+                answerText = `Где-то ошибка \n\u{1F60E}`
+            };
+
+            const answerPhrase = answerText;
             answerField.innerText = answerPhrase;
             gameRun = false;
         } else {
-            minValue = answerNumber  + 1;
-            answerNumber  = Math.floor((minValue + maxValue) / 2);
+            minValue = answerNumber + 1;
+            answerNumber = Math.floor((minValue + maxValue) / 2);
             orderNumber++;
             orderNumberField.innerText = orderNumber;
-            answerField.innerText = `Вы загадали число ${answerNumber }?`;
+            const phraseRandom = Math.round(Math.random() * 3);
+            if (phraseRandom == 1) {
+                answerField.innerText = `Вы загадали число ${answerNumber }`
+            } else if (phraseRandom == 2) {
+                answerField.innerText = `Наверное это ${answerNumber }`
+            } else {
+                answerField.innerText = `Думаю это число ${answerNumber }`
+            };
         }
     }
 })
 
-document.getElementById('btnEqual').addEventListener('click', function () {
-    if (gameRun){
-        answerField.innerText = `Я всегда угадываю\n\u{1F60E}`
-        gameRun = false;
+// Меньше
+document.getElementById('btnLess').addEventListener('click', function () {
+    if (gameRun) {
+        if (minValue + 1 === maxValue) {
+            const phraseRandom = Math.round(Math.random() * 3);
+            let answerText;
+            if (phraseRandom == 1) {
+                answerText = `Что-то не то\n\u{1F60E}`
+            } else if (phraseRandom == 2) {
+                answerText = `Что-то здесь не так \n\u{1F60E}`
+            } else {
+                answerText = `Где-то ошибка \n\u{1F60E}`
+            };
+
+            const answerPhrase = answerText;
+            answerField.innerText = answerPhrase;
+            gameRun = false;
+        } else {
+            maxValue = answerNumber - 1;
+            answerNumber = Math.floor((minValue + maxValue) / 2);
+            orderNumber++;
+            orderNumberField.innerText = orderNumber;
+            const phraseRandom = Math.round(Math.random() * 3);
+            if (phraseRandom == 1) {
+                answerField.innerText = `Вы загадали число ${answerNumber }`
+            } else if (phraseRandom == 2) {
+                answerField.innerText = `Наверное это ${answerNumber }`
+            } else {
+                answerField.innerText = `Думаю это число ${answerNumber }`
+            };
+        }
     }
 })
 
+// Верно
+document.getElementById('btnEqual').addEventListener('click', function () {
+    if (gameRun) {
+        const phraseRandom = Math.round(Math.random() * 3);
+        let answerText;
+        if (phraseRandom == 1) {
+            answerText = `Я всегда угадываю\n\u{1F60E}`
+        } else if (phraseRandom == 2) {
+            answerText = `Я молодец? \n\u{1F60E}`
+        } else {
+            answerText = `Ты думал, что я тебя не переиграю? \n\u{1F60E}`
+        };
+
+        const answerPhrase = answerText;
+        answerField.innerText = answerPhrase;
+        gameRun = false;
+    }
+})
